29cd67568b43ccb51ccef6f6192fbfd10935b9c9
majonga88 <majonga.agnojam@gmail.com>
2023-08-25 10:12:31 +0200

    update

--- component/Icon/Copyright.tsx
+++ component/Icon/Copyright.tsx
@@ -1,13 +1,19 @@
 import { Box, Text } from "@chakra-ui/react";
+import { League_Spartan } from "next/font/google";
 
 interface Props {
   logoColor: string;
 }
 
+const ls400 = League_Spartan({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export function Copyright({ logoColor }: Props) {
   return (
     <Box float="right">
-      <Text position="fixed" bottom="4" color={logoColor}>
+      <Text position="fixed" bottom="4" color={logoColor} fontSize="2rem" className={ls400.className}>
         ©
       </Text>
     </Box>
