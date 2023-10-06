8b5139aaf5070b95a06cb5d322f205432cc21998
majonga88 <majonga.agnojam@gmail.com>
2023-10-06 20:04:32 +0200

    update

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Julian. Photography: Elegance, Sophistication, and Iconic Flair in Every Frame</title>
+      <meta name="description" content="Explore Julian's photography, where elegance meets art in capturing timeless moments across global terrains and cultures."/>
+      <meta name="keywords" content="Julian, photography, elegance, global, timeless moments, cultures, terrains, artistic expression"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   );
